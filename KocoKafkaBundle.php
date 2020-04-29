14c1008b49166e75666877eb8d47cada2432c71f
Konstantin Scheumann <konstantin@konstantin.codes>
2020-04-30 00:38:50 +0200

    rm unused Import

--- KocoKafkaBundle.php
+++ KocoKafkaBundle.php
@@ -2,8 +2,6 @@
 
 namespace Koco\Kafka;
 
-use Koco\Kafka\DependencyInjection\Compiler\DecoderCompilerPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KocoKafkaBundle extends Bundle
